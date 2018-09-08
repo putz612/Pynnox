2d198853f1ccd898c22e0b2e7e07c32200e1fdab
Jason Sievert <jsievert@gmail.com>
2018-09-08 08:49:38 -0500

    Now able to get systems

--- pynnox_api.py
+++ pynnox_api.py
@@ -2,19 +2,28 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
 
 class lennox_thermostat():
     """Some cool description here"""
-    def __init__(self, username, password, system, zone):
+    def __init__(self, username, password):
         self._name = 'lennox'
         self._username = username
         self._password = password
-        
+        self.systems = "Unknown"
         self._system = system
         self._zone = zone
         self._serialNumber = "Unknown"
+        self._baseURL = "https://services.myicomfort.com/DBAcessService.svc/"
+
 
     def get(self):
-        url = "https://services.myicomfort.com/DBAcessService.svc/"
-        systemInfo = requests.get(url + "GetSystemsInfo?UserId=" + self._username, auth=HTTPBasicAuth(self._username, self._password))
-        print ( systemInfo.text)
+        systemInfo = requests.get(self._baseURL + "GetSystemsInfo?UserId=" + self._username, auth=HTTPBasicAuth(self._username, self._password))
+        pp.pprint( systemInfo.json())
+    def getSystems(self):
+        systemInfo = requests.get(self._baseURL + "GetSystemsInfo?UserId=" + self._username, auth=HTTPBasicAuth(self._username, self._password))
+        systems = systemInfo.json()
+        pp.pprint( systems["Systems"])
+        self.systems = systems["Systems"]
--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 from pynnox_api import lennox_thermostat
-
+import pprint
 import config
+
+pp = pprint.PrettyPrinter(indent=4)
 
 username = config.username
 password = config.password
 
-api = lennox_thermostat(username, password, 0, 0)
-api.get()
+api = lennox_thermostat(username, password)
+api.getSystems()
+
+print("Trying API")
+
+print(api.systems[0]["Gateway_SN"])
